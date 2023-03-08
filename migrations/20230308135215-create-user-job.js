d631cdbfcc906991af7292ca72916bab468221f7
sulthanna97 <sulthannaufal1997@gmail.com>
2023-03-08 20:58:08 +0700

    migration doang

---
+++ migrations/20230308135215-create-user-job.js
@@ -0,0 +1,39 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('UserJobs', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      JobId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Jobs',
+          key: 'id'
+        },
+      },
+      UserId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id'
+        },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('UserJobs');
+  }
+};
